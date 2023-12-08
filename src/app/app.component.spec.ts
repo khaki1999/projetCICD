04675112951f7f61e1e968971a717fddcc8ec882
Djomou _rick <rickkhaki@gmail.com>
2023-12-08 15:15:16 +0100

    mise a jour

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,5 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
- 
+  it(`should have as title 'projetCICD'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('projetCICD');
+  });
+
+
 });
