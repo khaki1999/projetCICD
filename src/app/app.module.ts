ce26ba644e18ed2a35913bd530f2d8ed94934298
Djomou _rick <rickkhaki@gmail.com>
2023-12-08 15:07:02 +0100

    mise a jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AccueilComponent } from './component/accueil/accueil.component';
-import { AboutComponent } from './component/about/about.component';
+
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms';
 
@@ -14,8 +14,7 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
 
 
-    AccueilComponent,
-    AboutComponent
+
   ],
   imports: [
     BrowserModule,
