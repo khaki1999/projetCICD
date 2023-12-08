22f32f37589aa7d45d4c1203b81d6555d13deebc
Djomou _rick <rickkhaki@gmail.com>
2023-12-08 16:29:48 +0100

    mise a jour

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('projetCICD');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projetCICD app is running!');
-  });
 
 
 });
