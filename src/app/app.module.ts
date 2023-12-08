a0c6936e3da1bab41d82f0a131f2b45a3dd2a5d8
Djomou _rick <rickkhaki@gmail.com>
2023-12-08 14:58:04 +0100

    mise a jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
+
 import { AccueilComponent } from './component/accueil/accueil.component';
 import { AboutComponent } from './component/about/about.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
@@ -13,8 +12,8 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
+
+
     AccueilComponent,
     AboutComponent
   ],
